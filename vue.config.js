b48b07c069b611a2b10569e10736ca28b08b9467
wuzhe-redmi <wuzhe_@aliyun.com>
2020-12-17 23:14:09 +0800

    配置vue-cli自动打开浏览器

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ function resolve (dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
+  devServer: {
+    open: true
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
