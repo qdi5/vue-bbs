21391b27aa705dab19f7b842d8d2a6478e64d76c
wuzhe-redmi <wuzhe_@aliyun.com>
2021-02-21 22:22:55 +0800

    axios超时时间为10分钟；注册路由beforeEnter逻辑修改；注册确认密码验证修改

--- src/utils/veevalidate.js
+++ src/utils/veevalidate.js
@@ -2,6 +2,7 @@ import { extend, localize } from 'vee-validate'
 import { required, email, min, max, length, confirmed } from 'vee-validate/dist/rules'
 import zh from 'vee-validate/dist/locale/zh_CN.json'
 
+// 引入验证规则
 extend('email', email)
 extend('min', min)
 extend('max', max)
@@ -11,6 +12,7 @@ extend('confirmed', confirmed)
 localize('zh_CN', {
   messages: {
     ...zh.messages,
+    // 定义required规则的错误提示
     required: '请输入{_field_}'
   },
   names: {
@@ -20,8 +22,10 @@ localize('zh_CN', {
     username: '昵称',
     vercode: '验证码',
     repassword: '确认密码'
+
   },
   fields: {
+    // 定义字段名的各个验证规则的错误提示
     email: {
       email: '请输入正确的{_field_}',
       required: '请输入{_field_}!!!'
